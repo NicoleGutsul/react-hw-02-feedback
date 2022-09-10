fc82792711531a762dfbe0f1e63c009ef2b8c10d
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-10 20:18:06 +0100

    adds state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-export const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+}
